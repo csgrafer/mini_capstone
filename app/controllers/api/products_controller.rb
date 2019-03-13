504f2affca8b580ad3f2bca0e51489f2edba9498
“YOUR <2drum4fun@gmail.com>
2019-03-13 07:40:37 -0500

    add routes, model, and views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,58 @@
 class Api::ProductsController < ApplicationController
+  def index
+    if params[:search]
+      @products = Product.where("name LIKE ?", "%#{params[:search]}%")
+    else
+      @products = Product.all
+    end
+
+    if params[:discount] == "true"
+      @products = @products.where("price < ?", 100)
+    end
+
+    if params[:sort] && params[:sort_order]
+      @products = @products.order(params[:sort] => params[:sort_order])
+    end
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render 'errors.json.jbuilder', status: :unprocessible_entity
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render 'errors.json.jbuilder', status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {message: "Product successfully destroyed"}
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 10..500 }
+
+  def is_discounted?
+    return true if price < 10
+    return false
+  end
+
+  def tax
+    return (price * 0.09).round(2)
+  end
+
+  def total
+    return (price + tax).round(2)
+  end
+
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
+
 end
